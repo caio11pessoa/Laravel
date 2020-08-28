d7099d2c4092226ae6b30b6fb54a5e18494242de
Caio Pessoa <caio.almeida.pessoa07@aluno.ifce.edu.br>
2020-08-28 08:46:01 -0300

    Adicionando aula template1 template2 e declarando estilos

--- projetoLaravelEstudo/resources/views/clientes/index.blade.php
+++ projetoLaravelEstudo/resources/views/clientes/index.blade.php
@@ -1,28 +1,36 @@
-<h3>{{$titulo}}:</h3>
-<a href="{{ route('clientes.create')}}">NOVO CLIENTE</a>
-@if(count($clientes)>0)
-<ul>
-    @foreach ($clientes as $c)
-        <li>
-        {{ $c['id'] }} | {{ $c['nome'] }} |
-        <a href="{{ route('clientes.edit', $c['id'] )}}">EDITAR</a>|
-        <a href="{{ route('clientes.show', $c['id'] )}}">info</a>
-        <form action="{{ route('clientes.destroy', $c['id'] )}}" method="POST">
-            @csrf
-            @method('DELETE')
-            <input type="submit" value="Apagar">
-        </form>
-    </li>
-    @endforeach
-    
-</ul>
+@extends('layouts.principal')
 
-@else
+@section('conteudo')
 
-<h4>Não existe usuários cadastrados.</h4>
+    <h3>{{$titulo}}:</h3>
+    <a href="{{ route('clientes.create')}}">NOVO CLIENTE</a>
 
-@endif
+    @if(count($clientes)>0)
 
-@empty($clientes)
-    <h4>Não existem clientes cadastrados.</h4>
-@endempty
+    <ul>
+        @foreach ($clientes as $c)
+            <li>
+            {{ $c['id'] }} | {{ $c['nome'] }} |
+            <a href="{{ route('clientes.edit', $c['id'] )}}">EDITAR</a>|
+            <a href="{{ route('clientes.show', $c['id'] )}}">info</a>
+            <form action="{{ route('clientes.destroy', $c['id'] )}}" method="POST">
+                @csrf
+                @method('DELETE')
+                <input type="submit" value="Apagar">
+            </form>
+        </li>
+        @endforeach
+        
+    </ul>
+
+    @else
+
+    <h4>Não existe usuários cadastrados.</h4>
+
+    @endif
+
+    @empty($clientes)
+        <h4>Não existem clientes cadastrados.</h4>
+    @endempty
+
+@endsection
---
+++ projetoLaravelEstudo/resources/views/layouts/principal.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Views</title>
+    <link rel="stylesheet" href="{{ asset('css/principal.css')}}">
+
+</head>
+<body>
+    <div class="row">
+        <div class="co1">
+            <div class="menu">
+                <ul>
+                    <li><a href="{{route('clientes.index')}}">Clientes</a></li>
+                    <li><a href="">Produtos</a></li>
+                    <li><a href="">Departamentos</a></li>
+                </ul>
+            </div>
+        </div>
+        <div class="col2">
+            @yield('conteudo')
+        </div>
+    </div>
+    
+</body>
+</html>
